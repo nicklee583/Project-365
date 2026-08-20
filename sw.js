73eca6821ea06b50b5577bb240b0149fcbde4ef9
nicklee583 <nicklee583@gmail.com>
2026-08-19 23:38:44 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='365-project-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./data.js','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
